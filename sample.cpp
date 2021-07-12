d63b3434e0312e0516a018d96475ba062178edb8
Walkover <walkover@Samars-MacBook-Pro.local>
2021-07-12 13:27:32 +0530

    12 jul 21 | 03 | residue files

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     char ch = 'e';
-    switch(ch) {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u': cout<<"Vowel"<<endl;
-        default: cout<<"Consonant"<<endl;
+    switch (ch)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        cout << "Vowel" << endl;
+        break;
+    default:
+        cout << "Consonant" << endl;
     }
     return 0;
 }
